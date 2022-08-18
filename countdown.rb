b55a948b616bea081764e0ce16514cde0a10ae5b
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-18 08:03:23 +0300

    Lab Complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
+require 'pry'
 #write your code here
 
-def countdown
+def countdown (number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (num_sec)
+    while num_sec > 0
+        puts "#{num_sec} SECOND(S)!"
+        sleep 1
+        num_sec -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
