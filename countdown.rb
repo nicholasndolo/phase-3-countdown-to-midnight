01289f44f01bb592648632e4f6adc0894da67de9
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-18 08:05:53 +0300

    commit tree b50d17271346b475f3c75790ea292d5a0ca310a6

--- countdown.rb
+++ countdown.rb
@@ -12,8 +12,8 @@ end
 def countdown_with_sleep (num_sec)
     while num_sec > 0
         puts "#{num_sec} SECOND(S)!"
-        sleep 1
         num_sec -= 1
+        sleep 1
     end
     "HAPPY NEW YEAR!"
 end
